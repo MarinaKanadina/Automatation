1ca046b92064530c4748e1b84aecc4a1a52b91ee
MarinaKanadina <kanadinamarina@gmail.com>
2024-06-24 22:22:41 +0300

    HW2

---
+++ Lesson2/lesson_2_task_0.py
@@ -0,0 +1,5 @@
+my_age = input('Ваш возраст: my_age')
+print(my_age)
+my_age = (42)
+my_age = my_age + 1
+print(my_age)
---
+++ Lesson2/lesson_2_task_8.py
@@ -0,0 +1,3 @@
+l = list(range(18, 0, -4))
+list(range(18, 0, -4)) 
+print(l)
